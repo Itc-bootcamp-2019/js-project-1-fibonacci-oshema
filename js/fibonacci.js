c47c6888f1e301dc6d2fe457abb345fab2757944
Ori <oorishema@gmail.com>
2020-03-04 12:21:45 +0200

    milestone 4 completed

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,15 +1,21 @@
 
-function fibonacci(x) {
-    let num0 = 0;
-    let num1 = 1;
-    let y = 0;
-    for (let i = 1; i < x; i++) {
-        num0 = y;
-        y = num1 + num0;
-        num1 = num0;
+function getInputValue() {
+    var inputVal = document.getElementById("myInput").value;
+
+    function fibonacci(x) {
+        let num0 = 0;
+        let num1 = 1;
+        let y = 0;
+        for (let i = 1; i < x; i++) {
+            num0 = y;
+            y = num1 + num0;
+            num1 = num0;
+        }
+        document.getElementById("result").innerText = y;
     }
-    document.getElementById('y').innerText = y;
+
+
+    fibonacci(inputVal);
 }
-x = 10;
-fibonacci(x);
-document.getElementById('x').innerText = x;
+
+
